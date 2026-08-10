b9bf67584ad0eb5a8d3a37eaa1a502c5fae5bf6f
JC Abucay <289777943+abukix@users.noreply.github.com>
2026-08-10 20:47:00 +0800

    Add sandbox/python Day 5: Roster operations stub

---
+++ sandbox/python/day05_roster_ops.py
@@ -0,0 +1,35 @@
+"""
+Day 5 — Roster operations (list ops, no mutation of input)
+
+Add:
+    add_member(party, character) -> list[dict]
+    remove_member(party, name) -> bool   # True if a member was removed
+    sort_by_level(party) -> list[dict]   # does not mutate input
+    filter_alive(party) -> list[dict]    # does not mutate input
+"""
+
+from day04_roster import print_roster  # noqa: F401
+
+
+def add_member(party, character):
+    # TODO
+    pass
+
+
+def remove_member(party, name):
+    # TODO: return True/False depending on whether a member was removed
+    pass
+
+
+def sort_by_level(party):
+    # TODO: return a new sorted list, don't mutate party
+    pass
+
+
+def filter_alive(party):
+    # TODO: return a new list of only alive members, don't mutate party
+    pass
+
+
+if __name__ == "__main__":
+    pass
